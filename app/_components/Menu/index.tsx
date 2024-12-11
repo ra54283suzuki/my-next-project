e5651ae43740138e608d69484479e2382ed48ff7
Haruto Suzuki <ra54283suzuki@gmail.com>
2024-12-11 23:31:26 +0900

    4-4-2

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./index.module.css";
 
 export default function Menu() {
+  const open = () => {
+    document.querySelector("nav")?.classList.add(styles.open);
+  };
   return (
     <div>
       <nav className={styles.nav}>
@@ -18,7 +23,7 @@ export default function Menu() {
           </li>
         </ul>
       </nav>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={open}>
         <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
       </button>
     </div>
