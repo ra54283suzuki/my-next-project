66fb9b264ef8528dd6624352e7c8cf3132268b09
Haruto Suzuki <ra54283suzuki@gmail.com>
2024-12-11 22:02:11 +0900

    4-3-2

---
+++ app/members/layout.tsx
@@ -0,0 +1,9 @@
+import Sheet from "@/app/_components/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return <Sheet>{children}</Sheet>;
+}
