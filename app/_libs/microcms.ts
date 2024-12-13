f7cc705df8c7f8bc468a9986e95d7517701e4afb
Haruto Suzuki <ra54283suzuki@gmail.com>
2024-12-13 20:58:40 +0900

    5-4-2

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -14,17 +14,15 @@ export type Member = {
 
 export type Category = {
   name: string;
-};
+} & MicroCMSListContent;
 
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
@@ -45,4 +43,11 @@ export const getMembersList = async (queries?: MicroCMSQueries) => {
     queries,
   });
   return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
 };
